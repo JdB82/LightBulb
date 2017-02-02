66791c567d1803a10a561cb7afab92c93e8519a8
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-02 17:06:43 +0100

    Initial Commit

---
+++ LightBulb/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  LightBulb
+//
+//  Created by Jeroen de Bie on 02/02/2017.
+//  Copyright © 2017 Jeroen de Bie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var BulbOnOff: UIImageView!
+    var lightsOnOff: Bool  = true
+    var flickerController: Bool = true
+    var Flickerimages: Array = [#imageLiteral(resourceName: "LightBulbOff"), #imageLiteral(resourceName: "LightBulbOn")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func LightButtonOnOff(_ sender: Any) {
+        if lightsOnOff {
+            lightsOnOff = false
+            BulbOnOff.image = #imageLiteral(resourceName: "LightBulbOn")
+        } else {
+            lightsOnOff = true
+            BulbOnOff.image = #imageLiteral(resourceName: "LightBulbOff")
+        }
+    }
+
+    @IBAction func FlickerButton(_ sender: Any) {
+        if flickerController == true {
+            BulbOnOff.animationImages = Flickerimages
+            BulbOnOff.animationDuration = 0.2
+            BulbOnOff.animationRepeatCount = 10000
+            BulbOnOff.startAnimating()
+            flickerController = false
+        } else {
+            flickerController = true
+            BulbOnOff.stopAnimating()
+        }
+    }
+
+}
+
